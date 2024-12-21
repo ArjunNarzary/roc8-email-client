a1382a8bfcbd69b0b431869ecb5c32542201e406
Arjun Narzary <arjunn@byteridge.com>
2024-12-21 13:00:24 +0530

    First code setup

---
+++ README.md
@@ -0,0 +1,14 @@
+# Email client UI
+
+## Libraries used
+
+- Vite
+- TypeScript
+- TailwindCss
+
+## Run below commands to run the project
+
+```js
+npm install
+npm run dev
+```
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      accent: "#E54065",
+      backgroundColor: "#F4F5F9",
+      borderColor: "#CFD2DC",
+      textColor: "#636363",
+      filterColor: "#E1E4EA",
+      readBackground: "#F2F2F2"
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
